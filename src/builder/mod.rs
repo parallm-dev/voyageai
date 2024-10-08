4c95fd99a4f0524188a8a4339b2b2ed9f87a3f2d
David Maple <david@cloudsamur.ai>
2024-10-08 12:21:53 -0700

    feat: Add EmbeddingsRequest and RerankRequest exports

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -4,4 +4,6 @@ mod voyage;
 
 pub use embeddings::EmbeddingsRequestBuilder;
 pub use rerank::RerankRequestBuilder;
+pub use voyage::EmbeddingsRequest;
+pub use voyage::RerankRequest;
 pub use voyage::VoyageBuilder;
